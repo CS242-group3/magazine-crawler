230eff52703ad1f230631ef442b5470df1e9dc80
CS242-group3 <77935329+CS242-group3@users.noreply.github.com>
2021-01-24 11:17:23 -0800

    Add files via upload

---
+++ cover_crawler.py
@@ -0,0 +1,18 @@
+import scrapy
+import re
+
+class MagazineSpider(scrapy.Spider):
+    name = 'covers'
+
+    start_urls = ["http://www.coverbrowser.com/covers/wolverines"]
+
+    def parse(self, response):
+        for cover in response.css('p.cover'):
+            yield{
+                'title' : cover.css('img::attr(alt)').get(),
+                'cover' : cover.css('img::attr(src)').get()
+            }
+
+        page_links = response.css('p.issuesNavigationBottom a::attr(href)').getall()
+        next_page = page_links[-1]
+        yield response.follow(next_page, callback=self.parse)
