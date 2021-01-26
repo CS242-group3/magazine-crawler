8dc36e686158074a207c6717d526a57b38430fca
CS242-group3 <77935329+CS242-group3@users.noreply.github.com>
2021-01-25 18:56:04 -0800

    Add files via upload

---
+++ items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MagazineCover(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    images = scrapy.Field()
+    images_urls = scrapy.Field()
